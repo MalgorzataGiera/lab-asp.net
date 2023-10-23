72eee35e8728bdcd10731d067797f44b1494d3f4
unknown <gosgie02@wp.pl>
2023-10-24 01:14:20 +0200

    dodano opcję tworzenia rezerwacji

---
+++ laboratorium 3 - App/Controllers/ReservationController.cs
@@ -0,0 +1,31 @@
+﻿using laboratorium_3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laboratorium_3___App.Controllers
+{
+    public class ReservationController : Controller
+    {
+        static Dictionary<int, Reservation> _reservations = new Dictionary<int, Reservation>();
+        static int index = 1;
+        
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Reservation model)
+        {
+            if (ModelState.IsValid)
+            {
+                // zapisz obiekt do bazy/kolekcji albo wykonaj operację
+                model.Id = index++;
+                _reservations[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ laboratorium 3 - App/Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace laboratorium_3___App.Models
+{
+    public class Reservation
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Musisz wspiać datę")]
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Musisz wpisać miasto")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Musisz podać adres")]
+        public string Address { get; set; }
+
+        public int Room { get; set; }
+        [Required(ErrorMessage = "Musisz wpisać właściela")]
+        public string Owner { get; set; }
+        [Required(ErrorMessage = "Musisz wpisać cenę")]
+        public decimal Price { get; set; }
+    }
+
+}
