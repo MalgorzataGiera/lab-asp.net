593e414ef3bf51836b84ba21d5f4232b2ab95b1c
Małgorzata Giera <malgorzata.giera@microsoft.wsei.edu.pl>
2023-10-24 14:16:17 +0200

    dodano enum, metodę GetDisplayName(), zmieniono sposób wyświetlania w widoku index

--- laboratorium 3 - App/Models/Contact.cs
+++ laboratorium 3 - App/Models/Contact.cs
@@ -5,17 +5,22 @@ namespace laboratorium_3___App.Models
 {
     public class Contact
     {
+        [Display(Name = "Priorytet")]
+        public Priority Priority { get; set; }
         [HiddenInput]
         public int Id { get; set; }
+        [Display(Name = "Imię")]
         [Required(ErrorMessage = "Musisz wspiać imię")]
         [StringLength(maximumLength: 100, ErrorMessage = "Zbyt długie imię, maksymalnie 100 znaków")]
         public string Name { get; set; }
+        [Display(Name = "E-mail")]
         [Required(ErrorMessage = "Musisz podać adres e-mail")]
         [EmailAddress(ErrorMessage = "Niepoprawny adres email, brak znaku @")]
         public string Email { get; set; }
+        [Display(Name = "Telefon")]
         [Phone(ErrorMessage = "Niepoprawny numer telefonu, wpisz cytry!")]
         public string Phone { get; set; }
-
+        [Display(Name = "Data urodzenia")]
         public DateTime Birth { get; set; }
     }
 
