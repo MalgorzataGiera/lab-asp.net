34a5fcd937f60e1791cd9b0aec0dd6458986c41a
unknown <gosgie02@wp.pl>
2023-11-06 22:57:31 +0100

    Zdefiniowano typ enum z atrybutami Display dla pokoi do wyboru

---
+++ laboratorium 3 - App/Models/Rooms.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace laboratorium_3___App.Models
+{
+    public enum Rooms
+    {
+        [Display(Name = "Room 1")]
+        Room1 = 1,
+
+        [Display(Name = "Room 2")]
+        Room2 = 2,
+
+        [Display(Name = "Room 3")]
+        Room3 = 3,
+
+        [Display(Name = "Room 4")]
+        Room4 = 4,
+
+        [Display(Name = "Room 5")]
+        Room5 = 5
+    }
+}
