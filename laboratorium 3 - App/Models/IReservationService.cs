7fd7316236b45b6e5138dcb34b75bda9756ba171
unknown <gosgie02@wp.pl>
2023-11-06 23:27:14 +0100

    Zdefiniowano i zaimplementowana serwis dla konrolera reservation

---
+++ laboratorium 3 - App/Models/IReservationService.cs
@@ -0,0 +1,11 @@
+﻿namespace laboratorium_3___App.Models
+{
+    public interface IReservationService
+    {
+        int Add(Contact book);
+        void Delete(int id);
+        void Update(Contact book);
+        List<Contact> FindAll();
+        Contact? FindById(int id);
+    }
+}
---
+++ laboratorium 3 - App/Models/MemoryReservationService.cs
@@ -0,0 +1,34 @@
+﻿namespace laboratorium_3___App.Models
+{
+    public class MemoryReservationService
+    {
+        private Dictionary<int, Reservation> _items = new Dictionary<int, Reservation>();
+        public int Add(Reservation item)
+        {
+            int id = _items.Keys.Count != 0 ? _items.Keys.Max() : 0;
+            item.Id = id + 1;
+            _items.Add(item.Id, item);
+            return item.Id;
+        }
+
+        public void Delete(int id)
+        {
+            _items.Remove(id);
+        }
+
+        public List<Reservation> FindAll()
+        {
+            return _items.Values.ToList();
+        }
+
+        public Reservation? FindById(int id)
+        {
+            return _items[id];
+        }
+
+        public void Update(Reservation item)
+        {
+            _items[item.Id] = item;
+        }
+    }
+}
